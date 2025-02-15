7486f7298cf146c9465598ce6c7f61a22d4e58eb
Nuhcan ATAR <nuhcanatar20@gmail.com>
2025-02-15 13:38:26 +0300

    Endpoints will be prepared for the profile.

--- routes/account.js
+++ routes/account.js
@@ -59,5 +59,89 @@ router.get("/account", async (req, res) => {
     }
 });
 
+router.put("/account-update", async (req, res) => {
+    try{
+        const token = req.headers.authorization?.split(' ')[1];
+        if(!token){
+            return res.status(401).json({ error: 'Token eksik' });
+        }
+
+        jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
+            if(err){
+                return res.status(403).json({ error: 'Geçersiz token' });
+            }
+
+            const userId = decoded.userId;
+            const {full_name, phone, address} = req.body;
+
+            if(!full_name || !phone || !address) {
+                return res.status(400).json({ error: 'Tüm alanları doldurunuz' });
+            }
+
+            try{
+                const [updateQuery] = await pool.query(
+                    `UPDATE users_detail 
+                     SET full_name = ?, phone = ?, address = ? 
+                     WHERE user_id = ?`, 
+                    [full_name, phone, address, userId]
+                );
+
+                if(updateQuery.affectedRows === 0){
+                    return res.status(404).json({ error: 'Kullanıcı detayları bulunamadı' });
+                }
+
+                return res.json({ message: 'Kullanıcı bilgileri başarıyla güncellendi' });
+            }catch (error){
+                return res.status(500).json({ error: 'Hata Oluştu!' });
+            }
+        });
+    }catch (error){
+        console.error(error);
+        res.status(500).json({ error: 'Bir hata oluştu' });
+    }
+});
+
+router.put("/account-city-district-update", async (req, res) => {
+    try{
+        const token = req.headers.authorization?.split(' ')[1];
+
+        if(!token){
+            return res.status(401).json({ error: 'Token eksik' });
+        }
+
+        jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
+            if(err){
+                return res.status(403).json({ error: 'Geçersiz token' });
+            }
+
+            const userId = decoded.userId;
+            const {city, district} = req.body;
+
+            if(!city ||!district) {
+                return res.status(400).json({ error: 'Şehir ve bölgeyi doldurunuz' });
+            }
+
+            try{
+                const [updateQuery] = await pool.query(
+                    `UPDATE users_detail 
+                     SET city = ?, district = ? 
+                     WHERE user_id = ?`, 
+                    [city, district, userId]
+                );
+
+                if(updateQuery.affectedRows === 0){
+                    return res.status(404).json({ error: 'Kullanıcı detayları bulunamadı' });
+                }
+
+                return res.json({ message: 'Kullanıcı şehir ve bölge bilgileri başarıyla güncellendi' });
+            }catch (error){
+                return res.status(500).json({ error: 'Hata Oluştu!' });
+            }
+        });
+    }catch (error){
+        console.error(error);
+        res.status(500).json({ error: 'Bir hata oluştu' });
+    }
+});
 
 module.exports = router;
